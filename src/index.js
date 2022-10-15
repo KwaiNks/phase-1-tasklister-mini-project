b893e28d63aaf4dc1c9bdd7b56e995d6dcdf8726
Richmond Nkansah <richmondnkansah@gmail.com>
2022-10-15 13:03:22 -0400

    working version of tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    //event listerner on form listeninf for an event and trigger and action(event handler)
+    document.querySelector("form").addEventListener("submit", (e) =>{
+     //prevents page from loading every single time
+      e.preventDefault();
+      //read value from testBox by targeting it specifically and take the value
+      const valueFromInput = e.target.new_task_description.value;
+      //get the list id
+      const ulList = document.getElementById("tasks")
+    
+  //appends a paragraph to a list
+      const newParagraphForEachToDo =  ulList.appendChild(document.createElement("p"));
+  
+      //appends the input from the user to the paragraph 
+      newParagraphForEachToDo.append(valueFromInput)
+    })
+
 });
